9a6a5c89799d7f8d3210acb41fd807ac38bf1aca
larry <quickexchange488@gmail.com>
2023-02-22 19:43:48 -0800

    9-times_table

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-
 int main(void)
+
 {
-    times_table();
-    return (0);
+	times_table();
+	return (0);
 }
