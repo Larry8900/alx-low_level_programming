0de6aa6f16128bbce2ede4af877010577c484dd7
larry <quickexchange488@gmail.com>
2023-03-04 22:48:04 -0800

    more pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,6 +11,7 @@
 char *_strcat(char *dest, char *src)
 
 {
+
 	int dlen = 0, i;
 
 	while (dest[dlen])
@@ -18,7 +19,7 @@ char *_strcat(char *dest, char *src)
 	dlen++;
 	}
 
-	for (i =0; src[i] != 0; i++)
+	for (i  = 0; src[i] != 0; i++)
 	{
 	dest[dlen] = src[i];
 	dlen++;
